9cfb2e119ef66aa9ee5221722b9022e16724aa62
Kevin Xu <49416599+kexu0741@users.noreply.github.com>
2019-04-14 22:16:54 -0600

    testing the csv
    csv info. Code works, csv works

---
+++ filetest.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+	ifstream in;
+	in.open("stats.csv");
+
+	string line;
+
+	//file format: PPG, FG%, 3PT%, FT%, APG, RPG, SPG, BPG, TOVPG, AST/TO ratio
+	//STL/TO (steals to turnovers), BLK/TO(Blocks to turnovers), scoring efficiency
+	//Shooting efficiency, personal fouls per game, Games played
+
+	while (getline(in, line)){
+		cout << line << endl; //should print every athlete with their respective stats, seperated by comma delim
+	}
+
+	return 0;
+}
