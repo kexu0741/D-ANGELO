bd98babd63ada5a9837b55985a396bbfbc926bfe
Kevin Xu <49416599+kexu0741@users.noreply.github.com>
2019-04-15 22:01:25 -0600

    Added player struct
    header file for use

---
+++ playerStruct.h
@@ -0,0 +1,29 @@
+#ifndef PLAYERSTRUCT_H
+#define PLAYERSTRUCT_H
+
+#include <string>
+
+struct player{
+	std::string name; //player name
+	std::string position; //position
+	int grade; //grade; 1-4 scale 
+	std::string college; //player's college 
+	double ppg; //points per game
+	double fgp; //field goal percentage
+	double threeptp; //3pt percentage
+	double ftp; //free throw percentage
+	double apg; //assists per game
+	double rpg; //rebounds per game
+	double spg; //steals per game
+	double bpg; //blocks per game
+	double tovpg; //turnovers per game
+	double astto; //assist to turnover ratio
+	double stlto; //steal to turnover ratio
+	double blkto; //block to turnover ratio
+	double sceff; //scoring efficiency
+	double sheff; //shooting efficiency
+	double pfpg; //personal fouls per game
+	int gamesPlayed; //games played
+};
+
+#endif
