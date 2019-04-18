3471bc8595cbbb2733e6183ada9087b838726155
Avnish94 <49416701+Avnish94@users.noreply.github.com>
2019-04-18 15:25:56 -0600

    all player struct attributes

--- BST.hpp
+++ BST.hpp
@@ -1,60 +1,75 @@
 #pragma once
 
 #include <string>
+using namespace std;
 
-
-// MovieNode: node struct that will be stored in the MovieTree BST
 
 struct PlayerNode
 {
-  std::string name; //player name
-	// std::string position; //position
-	// int grade; //grade; 1-4 scale
-	// std::string college; //player's college
-	// double ppg; //points per game
-	// double fgp; //field goal percentage
-	// double threeptp; //3pt percentage
-	// double ftp; //free throw percentage
-	// double apg; //assists per game
-	// double rpg; //rebounds per game
-	// double spg; //steals per game
-	// double bpg; //blocks per game
-	// double tovpg; //turnovers per game
-	// double astto; //assist to turnover ratio
-	// double stlto; //steal to turnover ratio
-	// double blkto; //block to turnover ratio
-	// double sceff; //scoring efficiency
-	// double sheff; //shooting efficiency
-	// double pfpg; //personal fouls per game
-	// int gamesPlayed; //games played
-  //
-	// double aggregateScore; //formula coming soon
+  string name; //player name
+	string position; //position
+	int grade; //grade; 1-4 scale
+	std::string college; //player's college
+	double ppg; //points per game
+	double fgp; //field goal percentage
+	double threeptp; //3pt percentage
+	double ftp; //free throw percentage
+	double apg; //assists per game
+	double rpg; //rebounds per game
+	double spg; //steals per game
+	double bpg; //blocks per game
+	double tovpg; //turnovers per game
+	double astto; //assist to turnover ratio
+	double stlto; //steal to turnover ratio
+	double blkto; //block to turnover ratio
+	double sceff; //scoring efficiency
+	double sheff; //shooting efficiency
+	double pfpg; //personal fouls per game
+	int gamesPlayed; //games played
+  double aggregateScore; //formula coming soon
 
 	PlayerNode* left = NULL;
 	PlayerNode* right = NULL;
 
-  // default constructor
   PlayerNode(){}
-  // Parametrized constructor
-  PlayerNode(std::string n)
+  PlayerNode(string n, string p, int g, string c, double p1, double f, double t, double f2, double a, double r, double s, double b ,double t2, double a2, double s2, double b2, double s3, double s4,double p2,  int g2, double a3)
   {
     name = n;
+    position = p;
+    grade = g;
+    college = c;
+    ppg = p1;
+    fgp = f;
+    threeptp = t;
+    ftp = f2;
+    apg = a;
+    rpg = r;
+    spg = s;
+    bpg = b;
+    tovpg = t2;
+    astto = a2;
+    stlto = s2;
+    blkto = b2;
+    sceff = s3;
+    sheff = s4;
+    pfpg = p2;
+    gamesPlayed = g2;
+    aggregateScore = a3;
   }
 };
 
 class PlayerTree
 {
 public:
-  // Check writeup for detailed function descriptions
   PlayerTree();
   ~PlayerTree();
-  void printMovieInventory();
-  void addMovieNode(std::string name);
-  void findMovie(std::string title);
-  void deleteNode(std::string title);
+  void printRoster();
+  void addPlayer(std::string name, string p, int g, string c, double p1, double f, double t, double f2, double a, double r, double s, double b ,double t2, double a2, double s2, double b2, double s3, double s4,double p2,  int g2, double a3);
+  void printPlayerStat(std::string name);
+  void deletePlayer(std::string name);
+  void printRosterTree( int space);
+  void printStat(string name);
 private:
-  //MovieNode *search(std::string title);
-  PlayerNode* deleteNodeHelper(PlayerNode *currNode, std::string value);
-  // Pointer to the root node
+  PlayerNode* deletePlayerHelper(PlayerNode *currNode, std::string value);
   PlayerNode *root;
 };
