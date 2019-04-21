c7d66cc6d0636c79f5a0b21ab2e66951ffa82cde
Avnish94 <49416701+Avnish94@users.noreply.github.com>
2019-04-21 01:31:37 -0600

    So it works with driver now

--- BST.hpp
+++ BST.hpp
@@ -10,10 +10,11 @@ public:
   PlayerTree();
   ~PlayerTree();
   void printRoster();
-  void addPlayer(std::string name, string p, int g, string c, double p1, double f, double t, double f2, double a, double r, double s, double b ,double t2, double a2, double s2, double b2, double s3, double s4,double p2,  int g2, double a3);
+  void addPlayer(string s[], double d[]);
   void printPlayerStat(std::string name);
   void deletePlayer(std::string name);
   void printRosterTree( int space);
+  player* findPlayer(string name);
 private:
   player* deletePlayerHelper(player *currNode, std::string value);
   void destroyRoster(player *currNode);
