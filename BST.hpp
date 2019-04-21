dd0fb71336c9007cd62d6227abd157277eb57fdf
Avnish94 <49416701+Avnish94@users.noreply.github.com>
2019-04-20 19:05:33 -0600

    BST uses the player struct now

--- BST.hpp
+++ BST.hpp
@@ -1,62 +1,8 @@
 #pragma once
-
+#include "playerStruct.h"
 #include <string>
 using namespace std;
 
-
-struct PlayerNode
-{
-  string name; //player name
-	string position; //position
-	int grade; //grade; 1-4 scale
-	std::string college; //player's college
-	double ppg; //points per game
-	double fgp; //field goal percentage
-	double threeptp; //3pt percentage
-	double ftp; //free throw percentage
-	double apg; //assists per game
-	double rpg; //rebounds per game
-	double spg; //steals per game
-	double bpg; //blocks per game
-	double tovpg; //turnovers per game
-	double astto; //assist to turnover ratio
-	double stlto; //steal to turnover ratio
-	double blkto; //block to turnover ratio
-	double sceff; //scoring efficiency
-	double sheff; //shooting efficiency
-	double pfpg; //personal fouls per game
-	int gamesPlayed; //games played
-  double aggregateScore; //formula coming soon
-
-	PlayerNode* left = NULL;
-	PlayerNode* right = NULL;
-
-  PlayerNode(){}
-  PlayerNode(string n, string p, int g, string c, double p1, double f, double t, double f2, double a, double r, double s, double b ,double t2, double a2, double s2, double b2, double s3, double s4,double p2,  int g2, double a3)
-  {
-    name = n;
-    position = p;
-    grade = g;
-    college = c;
-    ppg = p1;
-    fgp = f;
-    threeptp = t;
-    ftp = f2;
-    apg = a;
-    rpg = r;
-    spg = s;
-    bpg = b;
-    tovpg = t2;
-    astto = a2;
-    stlto = s2;
-    blkto = b2;
-    sceff = s3;
-    sheff = s4;
-    pfpg = p2;
-    gamesPlayed = g2;
-    aggregateScore = a3;
-  }
-};
 
 class PlayerTree
 {
@@ -69,7 +15,7 @@ public:
   void deletePlayer(std::string name);
   void printRosterTree( int space);
 private:
-  PlayerNode* deletePlayerHelper(PlayerNode *currNode, std::string value);
-  void destroyRoster(PlayerNode *currNode);
-  PlayerNode *root;
+  player* deletePlayerHelper(player *currNode, std::string value);
+  void destroyRoster(player *currNode);
+  player *root;
 };
